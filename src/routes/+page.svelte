22b95cc0b61e123c9355ef1aa2196b48a24ee459
hassan-jenny-internet <hassanh@jennyinternet.africa>
2023-07-14 12:11:04 +0200

    chart js

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { browser } from '$app/environment';
+	let xValues = ['Italy', 'France', 'Spain', 'USA', 'Argentina'];
+	let yValues = [55, 49, 44, 24, 15];
+	let barColors = ['#b91d47', '#00aba9', '#2b5797', '#e8c3b9', '#1e7145'];
+
+	const renderGrid = () => {
+		new Chart('myChart', {
+			type: 'pie',
+			data: {
+				labels: xValues,
+				datasets: [
+					{
+						backgroundColor: barColors,
+						data: yValues
+					}
+				]
+			},
+			options: {
+				title: {
+					display: true,
+					text: 'World Wide Wine Production 2018'
+				}
+			}
+		});
+	};
+</script>
+
+<svelte:head>
+	{#if browser}
+		<script
+			on:load={renderGrid}
+			src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"
+		></script>
+	{/if}
+</svelte:head>
+
+<canvas id="myChart" style="width:100%;max-width:600px" />
